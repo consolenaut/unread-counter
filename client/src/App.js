55cba89c895ad8a18ecf43c35b5db379f47189f0
Josh Black <josh@consolenaut.com>
2021-07-24 23:01:14 +0100

    Initial commit, forgive the sloppiness future-me

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import socketIOClient from "socket.io-client";
+
+const endpoint = "http://192.168.174.6:4001";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.div`
+  background-color: #000;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CountContainer = styled.div`
+  position: fixed;
+
+  top: 50%;
+  margin-top: -234px;
+  left: 50%;
+  margin-left: -251px;
+
+  width: 482px;
+  height: 482px !important;
+  background: ${({ unreads }) => unreads ? "#B70014" : "#000"};
+  border-radius: 50%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Count = styled.h1`
+  color: #fff;
+  font-size: 200px;
+  line-height: 1em;
+  text-align: center;
+`;
+
+const App = () => {
+  const [response, setResponse] = useState(false);
+  useEffect(() => {
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => setResponse(data));
+  }, []);
+
+  return (
+    <AppContainer>
+      <Header>
+        <CountContainer unreads={response}>
+          <Count>{response ? response : "?"}</Count>
+        </CountContainer>
+      </Header>
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require('cors');
+const http = require("http");
+const socketIo = require("socket.io");
+const index = require("./routes/index");
+const imaps = require('imap-simple');
+
+const PORT = process.env.PORT || 4001;
+
+const IMAPconnection;
+const unread;
+const ActiveSocket;
+
+const app = express();
+app.use(cors());
+app.options('*', cors());
+app.use(index);
+
+const getUnread = () => {
+  IMAPconnection.openBox('INBOX').then(() => {
+    const searchCriteria = ['UNSEEN'];
+
+    const fetchOptions = {
+      bodies: ['HEADER', 'TEXT'],
+      markSeen: false,
+    };
+
+    return IMAPconnection.search(searchCriteria, fetchOptions).then((results) => {
+      unread = results.length;
+      if (ActiveSocket) ActiveSocket.emit("FromAPI", unread);
+    });
+  });
+}
+
+const config = {
+  imap: {
+    user: 'josh@dotblack.io',
+    password: '123123234_Jj',
+    host: 'imappro.zoho.eu',
+    port: 993,
+    tls: true,
+    authTimeout: 3000,
+  },
+  onmail: () => getUnread(),
+  onupdate: () => getUnread(),
+};
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: { origin: '*' },
+});
+
+io.on("connection", (socket) => {
+  ActiveSocket = socket;
+  getUnread();
+  socket.on("disconnect", () => { ActiveSocket = undefined });
+});
+
+server.listen(PORT, () => {
+  imaps.connect(config).then(function (connection) {
+    IMAPconnection = connection;
+    getUnread();
+  });
+  console.log(`Listening on port ${PORT}`);
+});
